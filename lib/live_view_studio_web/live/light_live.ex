5040b9bd47c19439a99e33815b48f118a59f0a1b
Paul Fioravanti <paul.fioravanti@gmail.com>
2022-03-27 18:11:33 +1100

    Get Step 3 working

--- lib/live_view_studio_web/live/light_live.ex
+++ lib/live_view_studio_web/live/light_live.ex
@@ -17,13 +17,43 @@ defmodule LiveViewStudioWeb.LightLive do
           <%= @brightness %>%
         </span>
       </div>
-      <button>
+      <button phx-click="off">
         <img src="images/light-off.svg">
       </button>
-      <button>
+      <button phx-click="down">
+        <img src="images/down.svg">
+      </button>
+      <button phx-click="up">
+        <img src="images/up.svg">
+      </button>
+      <button phx-click="on">
         <img src="images/light-on.svg">
       </button>
     </div>
     """
+  end
+
+  @impl true
+  def handle_event("on", _params, socket) do
+    socket = assign(socket, :brightness, 100)
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("up", _params, socket) do
+    socket = update(socket, :brightness, &(&1 + 10))
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("down", _params, socket) do
+    socket = update(socket, :brightness, &(&1 - 10))
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("off", _params, socket) do
+    socket = assign(socket, :brightness, 0)
+    {:noreply, socket}
   end
 end
